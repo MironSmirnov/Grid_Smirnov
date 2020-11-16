c0313d47ba7149511a6236c809620def20de9bba
opilane <opilane@HP-CZC6297B8H>
2020-11-16 18:58:42 +0200

    not finished

--- Grid_Smirnov/Grid_Smirnov/MainPage.xaml.cs
+++ Grid_Smirnov/Grid_Smirnov/MainPage.xaml.cs
@@ -13,6 +13,7 @@ namespace Grid_Smirnov
     {
         TimePicker tpicker;
         Picker picker;
+        Entry entry;
         public MainPage()
         {
             Grid gr = new Grid
@@ -47,11 +48,73 @@ namespace Grid_Smirnov
             picker.Items.Add("Иду спать");
 
             gr.Children.Add(picker, 0, 0);
+            picker.PropertyChanged += Picker_PropertyChanged; ;
+        }
 
-            tpicker = new TimePicker()
+        private void Picker_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == TimePicker.TimeProperty.PropertyName)
             {
-                //Time = new TimeSpan(18,0,0)
-                Time
+                var time = tpicker.Time.Hours;
+                if (time == 7)
+                {
+                    entry.Text = "Просыпаюсь";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 8.30)
+                {
+                    entry.Text = "Учеба";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 16)
+                {
+                    entry.Text = "Дома";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 17)
+                {
+                    entry.Text = "Обед";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 18)
+                {
+                    entry.Text = "Прогулка";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 19)
+                {
+                    entry.Text = "Ужин";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 21)
+                {
+                    entry.Text = "Просмотр фильма";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 22)
+                {
+                    entry.Text = "Прогулка с собакой";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 23.30)
+                {
+                    entry.Text = "Подготовка ко сну";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 23.50)
+                {
+                    entry.Text = "Чтение книги";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 00)
+                {
+                    entry.Text = "Расстелание кровати";
+                }
+                else time = tpicker.Time.Hours;
+                if (time == 00.20)
+                {
+                    entry.Text = "Иду спать";
+                }
             }
         }
     }
